5a1540a8bb77eac93c0014b9d5b510c7cf8c9120
jasioCode <101008688+jasiuSYN@users.noreply.github.com>
2023-02-28 23:39:35 +0100

    Refactor models

--- thumbnails/api/models.py
+++ thumbnails/api/models.py
@@ -1,12 +1,23 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from django.core.validators import FileExtensionValidator
+from django.core.validators import (
+    FileExtensionValidator,
+    MaxValueValidator,
+    MinValueValidator,
+)
 
-# Create your models here.
+import os
+
+
+def get_user_directory_path(instance, filename):
+    user_id = instance.profile.user_id
+    return os.path.join("users", f"id_{str(user_id)}", filename)
 
 
 class ThumbnailSize(models.Model):
-    size = models.PositiveIntegerField()
+    size = models.PositiveIntegerField(
+        validators=[MinValueValidator(10), MaxValueValidator(720)]
+    )
 
     def __str__(self):
         return str(self.size)
@@ -14,9 +25,7 @@ class ThumbnailSize(models.Model):
 
 class Tier(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    thumbnail_size = models.ManyToManyField(
-        ThumbnailSize, related_name="thumbnail_sizes"
-    )
+    thumbnail_size = models.ManyToManyField(ThumbnailSize)
     allow_original_file_link = models.BooleanField(default=False)
     allow_expiring_links = models.BooleanField(default=False)
 
@@ -25,15 +34,25 @@ class Tier(models.Model):
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
-    tier = models.OneToOneField(Tier, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        get_user_model(), on_delete=models.CASCADE, related_name="profile"
+    )
+    tier = models.ForeignKey(Tier, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.user} {self.tier}"
 
+    def get_thumbnail_sizes(self):
+        sizes = self.tier.thumbnail_size.all()
+        return [size.size for size in sizes]
+
 
 class Image(models.Model):
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    profile = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name="images"
+    )
     image = models.ImageField(
-        blank=True, upload_to="", validators=[FileExtensionValidator(["jpeg", "png"])]
+        blank=True,
+        upload_to=get_user_directory_path,
+        validators=[FileExtensionValidator(["jpeg", "png"])],
     )
